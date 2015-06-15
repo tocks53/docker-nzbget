b400e6d281471b01d784ffaab9d7b4d1794d52a4
tocks53 <tocks0000@gmail.com>
2015-06-15 11:46:39 +0200

    Update nzbget.sh

--- nzbget.sh
+++ nzbget.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 umask 000
 
-exec nzbget -D -c /config/nzbget.conf
-
-while sleep 1; do
-  if [[ -z $(pgrep nzbget) ]]; then
-    break
-  fi
-done
+exec nzbget -s -c /config/nzbget.conf
